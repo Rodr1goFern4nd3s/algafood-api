f162a13975790b749dd985e298f70e90ab6305f7
Rodrigo Fernandes <fernandesrodrigo060@gmail.com>
2024-04-16 08:47:35 -0300

    implementado alguns query methods no repository

--- src/main/java/br/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -10,31 +10,42 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 
     @Autowired
-    private CozinhaRepository repository;
+    private CozinhaRepository cozinhaRepository;
 
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
 
     @GetMapping
     public List<Cozinha> listar(){
-        return repository.findAll();
+        return cozinhaRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long id){
 
-        Cozinha cozinha = repository.getReferenceById(id);
+        Cozinha cozinha = cozinhaRepository.getReferenceById(id);
 
         if(cozinha != null) {
             return ResponseEntity.ok(cozinha);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
     }
 
     @PostMapping
@@ -45,10 +56,10 @@ public class CozinhaController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
-        Cozinha cozinha = repository.getReferenceById(id);
+        Cozinha cozinha = cozinhaRepository.getReferenceById(id);
         System.out.println("Cozinha: " + cozinha);
         if(cozinha != null) {
-            repository.delete(cozinha);
+            cozinhaRepository.delete(cozinha);
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
--- src/main/java/br/com/algaworks/algafood/infrastructure/repository/CozinhaRepository.java
+++ src/main/java/br/com/algaworks/algafood/infrastructure/repository/CozinhaRepository.java
@@ -4,6 +4,13 @@ import br.com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+
+    List<Cozinha> findTodasByNome(String nome); //Pode trazer mais de uma cozinha com o mesmo nome e colocando em uma lista!
+    List<Cozinha> findTodasByNomeContaining(String nome); //Podemos fazer uma busca de um nome fazendo um like usando os %esquerda/direita% usando o Containing
+    Optional<Cozinha> findByNome(String nome);
 }
